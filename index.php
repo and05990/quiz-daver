58d217fe5acac2919ea94926123006bb044b8af4
and05990 <and05.quagliotti@gmail.com>
2024-03-06 12:40:48 +0100

    per daver

--- index.php
+++ index.php
@@ -6,6 +6,18 @@
     <title>Quiz-Daver</title>
 </head>
 <body>
-    
+    <div class="container">
+        <form action="selezionaquiz.php" method="post">
+            <div class="form-group">
+                <input type="text" name="username" id="username" placeholder="Inserisci l'username..." class="form-control" required>
+            </div>
+            <div class="form-group">
+                <input type="password" name="password" id="password" class="form-control" placeholder="Inserisci la password..." required>
+            </div>
+            <div class="form-group">
+                <button type="submit" class="btn btn-primary">Login</button>
+            </div>
+        </form>
+    </div>
 </body>
 </html>
---
+++ selezionaquiz.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sei pronto?</title>
+</head>
+<body>
+    <div class="container">
+        <h1>Vuoi comminciare il quiz?</h1>
+        <p></p>
+        <form action="quiz1.php" method="post">
+            <div class="form-group">
+                <button type="submit" class="btn btn-primary">Si</button>
+            </div>
+        </form>
+        <form action="index.php" method="post">
+            <div class="form-group">
+                <button type="submit" class="btn btn-primary">No</button>
+            </div>
+        </form>
+    </div>
+</body>
+</html>
